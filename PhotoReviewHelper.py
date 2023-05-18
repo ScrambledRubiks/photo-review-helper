665f79261ce484df14a0b136d2c49927c5c79ba1
ScrambledRubiks <89490229+ScrambledRubiks@users.noreply.github.com>
2023-05-18 11:00:02 -0600

    Added code comments making things more clear

--- PhotoReviewHelper.py
+++ PhotoReviewHelper.py
@@ -13,32 +13,32 @@ verticalRes = int(1696/2) #Vertical resolution for the photos' display, make sur
 renameTo = "_GOOD" #The suffix that pressing m will add to the file name
 #Once these are done you're all set!
 
-imagePath = list(imagePath)
+imagePath = list(imagePath) #This bit ensures that the file path will work whether or not there is a / at the end of the path, so "C:/ExamplePath/Photos/"
 if imagePath[len(imagePath)-1] == "/":
     imagePath.pop()
 imagePath = ''.join(imagePath)
 
 
-fullPath = "{}/{}{}{}.{}".format(imagePath, prefix,  str(imgNum), suffix, fileExtension)
+fullPath = "{}/{}{}{}.{}".format(imagePath, prefix,  str(imgNum), suffix, fileExtension) #Creates the full path for each photo
 
 win = tk.Tk()
 win.title("Photo Review Helper")
-path = list(os.path.dirname(os.path.abspath(__file__)))
+path = list(os.path.dirname(os.path.abspath(__file__))) #This stuff just gets the filepath for the icon
 for x in range(len(path)):
     if path[x] == "\\":
         path[x] = "/"
 path = ''.join(path)
-
 icon = tk.PhotoImage(file=f"{path}/ScrambledRubiks-Emblem.png")
 win.iconphoto(True,icon)
-image1 = Image.open(fullPath)
-image1 = image1.resize((int(2544/2), int(1696/2)), Image.LANCZOS)
+
+image1 = Image.open(fullPath) #These prepare the images for proper display
+image1 = image1.resize((int(2544/2), int(1696/2)), Image.LANCZOS) 
 ph = ImageTk.PhotoImage(image1)
 
 label1 = tk.Label(image=ph)
 label1.image = image1
 
-def key_pressed(event):
+def key_pressed(event): #This is everything that happens when you hit a key
     global imgNum
     global image1
     global fullPath
@@ -50,7 +50,7 @@ def key_pressed(event):
     global fileExtension
     global horizontalRes
     global verticalRes
-    if event.char == ",":
+    if event.char == ",": #Handling going down the photo list
         imgNum -= 1
         fullPath = "{}/{}{}{}.{}".format(imagePath, prefix,  str(imgNum), suffix, fileExtension)
         image1 = Image.open(fullPath)
@@ -59,7 +59,7 @@ def key_pressed(event):
             
         label1.configure(image=ph)
         label1.image = ph
-    elif event.char == ".":
+    elif event.char == ".": #Handling going up the photo list
         imgNum += 1 
         print(imgNum)
         fullPath = "{}/{}{}{}.{}".format(imagePath, prefix,  str(imgNum), suffix, fileExtension)
@@ -70,13 +70,13 @@ def key_pressed(event):
         label1.configure(image=ph)
         label1.image = ph
 
-    elif event.char == "m":
-        pathNoFileExtension = "{}/{}{}{}".format(imagePath, prefix,  str(imgNum), suffix)
+    elif event.char == "m": #Handling marking the photos
+        pathNoFileExtension = "{}/{}{}{}".format(imagePath, prefix,  str(imgNum), suffix) #This is to keep the file extension from being renamed like "photo1.jpg_GOOD"
         name = pathNoFileExtension + renameTo + "." + fileExtension
         os.rename(fullPath, name)
 
 
-# Position image
+#Position image
 label1.place(x=0, y=0)
 win.bind("<Key>",key_pressed)
 win.mainloop()
