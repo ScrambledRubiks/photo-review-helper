21d5f77973b6ffc7ddeeb44a251884a3158c893b
ScrambledRubiks <89490229+ScrambledRubiks@users.noreply.github.com>
2023-06-18 11:24:59 -0600

    Patch 1.3

--- PhotoReviewHelper.py
+++ PhotoReviewHelper.py
@@ -11,14 +11,13 @@ settingsPath = ''.join(settingsPath) + "/Setup.txt"
 setupFile = open(settingsPath, "r")
 s = setupFile.readlines()
 
-imgNum = int(s[3].rstrip()) #The rstrip removes newlines that are attached to everything
-imagePath = s[7].rstrip()
-prefix = s[11].rstrip()
-suffix = s[15].rstrip()
-fileExtension = s[19].rstrip()
-horizontalRes = int(s[23].rstrip())
-verticalRes = int(s[27].rstrip())
-renameTo = s[31].rstrip()
+imagePath = s[3].rstrip()
+fileExtension = s[7].rstrip()
+horizontalRes = int(s[11].rstrip())
+verticalRes = int(s[15].rstrip())
+renameTo = s[18].rstrip()
+
+
 
 
 
@@ -30,7 +29,6 @@ for x in range(len(imagePath)): #This corrects for backslashes used in the image
         imagePath[x] = "/"
 imagePath = ''.join(imagePath)
 
-fullPath = "{}/{}{}{}.{}".format(imagePath, prefix,  str(imgNum), suffix, fileExtension) #Creates the full path for each photo
 
 win = tk.Tk()
 win.title("Photo Review Helper")
@@ -43,50 +41,68 @@ path = ''.join(path)
 icon = tk.PhotoImage(file=f"{path}/ScrambledRubiks-Emblem.png")
 win.iconphoto(True,icon)
 
-print(fullPath)
-image1 = Image.open(fullPath) #These prepare the images for proper display
-image1 = image1.resize((int(2544/2), int(1696/2)), Image.LANCZOS) 
+file_path = []
+
+def get_image_files(image_folder): #ChatGPT helped with this stuff
+    global file_path
+    image_files = []
+    for file_name in os.listdir(image_folder):
+        if file_name.endswith(('.jpg', '.jpeg', '.png', '.gif')):
+            file_path = os.path.join(image_folder, file_name)
+            image_files.append(file_path)
+    image_files.sort(key=os.path.getmtime)  # Sort files by modification time
+    return image_files
+
+image_folder = imagePath
+image_files = get_image_files(image_folder)
+current_index = 0
+
+image1 = Image.open(image_files[0]) #These prepare the images for proper display
+image1 = image1.resize((horizontalRes, verticalRes), Image.LANCZOS) 
 ph = ImageTk.PhotoImage(image1)
 
 label1 = tk.Label(image=ph)
 label1.image = image1
 
-def key_pressed(event): #This is everything that happens when you hit a key
-    global imgNum
-    global image1
-    global fullPath
-    global label1
-    global ph
-    global imagePath
-    global prefix
-    global suffix
-    global fileExtension
-    global horizontalRes
-    global verticalRes
-    if event.char == ",": #Handling going down the photo list
-        imgNum -= 1
-        fullPath = "{}/{}{}{}.{}".format(imagePath, prefix,  str(imgNum), suffix, fileExtension)
-        image1 = Image.open(fullPath)
-        image1 = image1.resize((horizontalRes, verticalRes), Image.LANCZOS)
-        ph = ImageTk.PhotoImage(image1)
-            
-        label1.configure(image=ph)
-        label1.image = ph
-    elif event.char == ".": #Handling going up the photo list
-        imgNum += 1 
-        print(imgNum)
-        fullPath = "{}/{}{}{}.{}".format(imagePath, prefix,  str(imgNum), suffix, fileExtension)
-        image1 = Image.open(fullPath)
-        image1 = image1.resize((int(2544/2), int(1696/2)), Image.LANCZOS)
-        ph = ImageTk.PhotoImage(image1)
-            
-        label1.configure(image=ph)
-        label1.image = ph
 
+
+
+def show_image(image_path):
+    image = Image.open(image_path)
+    image = image.resize((horizontalRes, verticalRes), Image.Resampling.LANCZOS)
+    photo = ImageTk.PhotoImage(image)
+    label1.configure(image=photo)
+    label1.image = photo
+    label1.place(x=0, y=0) 
+
+
+def key_pressed(event):
+    global current_index
+    global file_path
+    global renameTo
+    
+    if event.char == ",":
+        current_index -= 1
+    elif event.char == ".":
+        current_index += 1
     elif event.char == "m": #Handling marking the photos
-        pathNoFileExtension = "{}/{}{}{}".format(imagePath, prefix,  str(imgNum), suffix) #This is to keep the file extension from being renamed like "photo1.jpg_GOOD"
-        name = pathNoFileExtension + renameTo + "." + fileExtension
-        os.rename(fullPath, name)
+        current_index = max(0, min(current_index, len(image_files) - 1))
+        
+        file_path = image_files[current_index]  # Update the file_path with the current image
+        
+        path2 = list(os.path.splitext(file_path)[0])
+        for x in range(len(path2)):
+            if path2[x] == "\\":
+                path2[x] = "/"
+        path2 = ''.join(path2)
+        
+        name = path2 + renameTo + "." + fileExtension
+        os.rename(file_path, name)
+    
+    current_index = max(0, min(current_index, len(image_files) - 1))
+    show_image(image_files[current_index])
+    print(current_index)
+
 
 
 #Position image
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Instructions:
 Open up Setup.txt and change the necessary lines.
 
 Operation:
-Left and right are < and >(, and .). To mark a photo that you like, press m. To find these photos, go into File Explorer and search for whatever you set in the variable renameTo(the default is "_GOOD").
+Left and right are < and >(, and .). To mark a photo that you like, press m. To find these photos, go into File Explorer and search for whatever you set  the renaming to be(the default is "_GOOD").
+Once you have marked a photo, the photo will not be viewable inside Photo Review Helper unless you close and reopen it.
